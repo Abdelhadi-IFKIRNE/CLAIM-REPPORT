49f87c76029c07624f5a9d651c6c30537b9cbe80
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-07 17:54:55 +0000

    update claim repport ...

---
+++ src/main/java/com/berexia/claimrepport/ClaimRepportApplication.java
@@ -0,0 +1,13 @@
+package com.berexia.claimrepport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimRepportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimRepportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berexia/claimrepport/ClaimRepportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berexia.claimrepport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimRepportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
